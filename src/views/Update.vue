260054fafa25439cb3ce0edb350ab5dd8ba372b3
adnan <adnan.syria.91@gmail.com>
2021-11-20 17:48:17 +0330

    update user

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -15,7 +15,7 @@
               <v-text-field
                 type="text"
                 v-model="form.Name"
-                placeholder="نام"
+                label="نام"
                 required
                 outlined
               />
@@ -25,41 +25,7 @@
               <v-text-field
                 type="text"
                 v-model="form.Family"
-                placeholder="نام خانوادگی"
-                required
-                outlined
-              />
-
-              <br />
-
-              <v-text-field
-                :append-icon="show4 ? 'mdi-eye' : 'mdi-eye-off'"
-                v-model="form.Password"
-                :type="show4 ? 'text' : 'password'"
-                required
-                placeholder="رمز عبور"
-                @click:append="show4 = !show4"
-                outlined
-              ></v-text-field>
-
-              <br />
-
-              <v-select
-                :items="Married"
-                label="وضعیت تاهل"
-                :item-text="'Name'"
-                :item-value="'Value'"
-                v-model="mar"
-                outlined
-                dense
-              >
-              </v-select>
-
-              <br />
-
-              <v-text-field
-                v-model="form.Mobile"
-                placeholder="شماره موبایل"
+                label="نام خانوادگی"
                 required
                 outlined
               />
@@ -68,28 +34,99 @@
 
               <v-text-field
                 v-model="form.BirthDate"
-                placeholder="تاریخ تولد"
+                label="تاریخ تولد"
                 required
                 outlined
               />
+
+              <br />
+
+               <v-select
+                class="select"
+                :items="Province"
+                :item-text="'Name'"
+                :item-value="'Id'"
+                type="text"
+                v-model="form.ProvinceId"
+                label="استان"
+                required
+                outlined
+                dense
+              >
+              </v-select>
+              <br />
+
+               <v-select
+                class="select"
+                :items="cities"
+                :item-text="'Name'"
+                :item-value="'Id'"
+                type="text"
+                v-model="form.CityId"
+                label="شهر"
+                required
+                outlined
+                dense
+              >
+              </v-select>
+
+              <br />
+
+           
+
+             
+
+              <v-text-field
+                v-model="form.Mobile"
+                label="شماره موبایل"
+                required
+                outlined
+              />
+
+              <v-select
+                :items="Degree"
+                label="تحصیلات"
+                :item-text="'Name'"
+                :item-value="'Value'"
+                v-model="form.Degree"
+                outlined
+                dense
+              >
+              </v-select>
+
+              <br />
+
+              <v-select
+                :items="JobType"
+                label="شغل"
+                :item-text="'Name'"
+                :item-value="'Value'"
+                v-model="form.JobType"
+                outlined
+                dense
+              >
+              </v-select>
 
               <br />
             </b-col>
 
             <b-col>
               <v-text-field
-                v-model="form.Degree"
-                type="integer"
-                placeholder="تحصیلات"
-                outlined
+                :append-icon="show4 ? 'mdi-eye' : 'mdi-eye-off'"
+                v-model="form.Password"
+                :type="show4 ? 'text' : 'password'"
                 required
-              />
+                label="رمز عبور"
+                @click:append="show4 = !show4"
+                outlined
+              ></v-text-field>
+
               <br />
 
               <v-text-field
-                v-model="form.JobType"
-                type="integer"
-                placeholder="شغل"
+                v-model="form.Address"
+                type="text"
+                label="آدرس"
                 outlined
                 required
               />
@@ -98,19 +135,32 @@
               <v-text-field
                 v-model="form.Email"
                 type="email"
-                placeholder="ایمیل"
+                label="ایمیل"
                 outlined
                 required
               />
               <br />
 
               <v-select
+                :items="Married"
+                label="وضعیت تاهل"
+                :item-text="'Name'"
+                :item-value="'Value'"
+                v-model="form.IsMarried"
+                outlined
+                dense
+              >
+              </v-select>
+              <br />
+
+                 <v-select
                 class="select"
                 :items="Gender"
                 :item-text="'Name'"
                 :item-value="'Value'"
                 type="text"
-                placeholder="جنسیت"
+                label="جنسیت"
+                v-model="form.Gender"
                 required
                 outlined
                 dense
@@ -118,53 +168,31 @@
               </v-select>
               <br />
 
-              <v-text-field
-                v-model="form.Address"
-                type="text"
-                placeholder="آدرس"
-                outlined
-                required
-              />
-              <br />
+             
 
-              <v-select
-                class="select"
-                :items="Province"
-                :item-text="'Name'"
-                :item-value="'Value'"
-                type="text"
-                v-model="form.ostan"
-                placeholder="استان"
-                required
-                outlined
-                dense
-              >
-              </v-select>
-              <br />
-
-              <v-select
-                class="select"
-                :items="showList"
-                :item-text="'Name'"
-                :item-value="'Value'"
-                type="text"
-                v-model="form.CityId"
-                placeholder="شهر"
-                required
-                outlined
-                dense
-              >
-              </v-select>
-              <br />
-
-              <v-file-input
-                :rules="rules"
+              <!-- <v-file-input
+                v-model="files"
                 accept="image/png, image/jpeg, image/bmp"
                 placeholder=" عکس پروفایل آپلود کنید"
                 prepend-icon=""
                 append-icon="mdi-camera"
                 outlined
-              ></v-file-input>
+              ></v-file-input> -->
+
+              <img :src="imageUrl" height="150" v-if="imageUrl" />
+              <v-text-field
+                label=" عکس پروفایل خود را آپلود کنید"
+                @click="pickFile"
+                v-model="imageName"
+                prepend-icon="attach_file"
+              ></v-text-field>
+              <input
+                type="file"
+                style="display: none"
+                ref="image"
+                accept="image/*"
+                @change="onFilePicked"
+              />
             </b-col>
           </b-row>
 
@@ -174,7 +202,7 @@
             elevation="5"
             rounded
             large
-          @click="updateCust()"
+            @click="updateCust()"
             variant="primary"
             >ثبت
           </v-btn>
@@ -205,33 +233,28 @@ import axios from "axios";
 export default {
   name: "Update",
   async created() {
-    // let rest = await axios.get(
-    //   `http://localhost:8080/api/Province/GetAll`,
+    let rest = await axios.get(`http://localhost:8080/api/Province/GetAll`, {
+      headers: {
+        token: localStorage.getItem("token"),
+      },
+    });
 
-    //   {
-    //     headers: {
-    //       token: localStorage.getItem("token"),
-    //     },
-    //   }
-    // );
-    // console.log(rest);
-    // this.Province = rest.data;
+    this.Province = rest.data;
 
-    //  let res = await axios.get(
-    //   `http://localhost:8080/api/City/GetByProvinceId/${this.form.ostan.id}`, this.form.ostan.id,
+    //  console.log('Province', this.Province);
 
-    //   {
-    //     headers: {
-    //       token: localStorage.getItem("token"),
-    //     },
-    //   }
-    // );
-    // console.log(res);
-    // this.cities = res.data;
+    //    let rest1 = await axios.get(`http://localhost:8080/api/City/GetAll`, {
+    //   headers: {
+    //     token: localStorage.getItem("token"),
+    //   },
+    // });
+
+    // this.cities = rest1.data;
+
+    //  console.log('cities', this.cities);
 
     let response = await axios.get(
       `http://localhost:8080/api/Customer/GetMyUser`,
-
       {
         headers: {
           token: localStorage.getItem("token"),
@@ -239,28 +262,22 @@ export default {
       }
     );
 
-    console.log(response);
+    console.log("Customer: ", response.data);
     this.form.Name = response.data.Name;
     this.form.Family = response.data.Family;
 
     this.form.Password = response.data.Password;
 
-    this.form.Gender = response.data.Gender;
-
     this.form.Mobile = response.data.Mobile;
 
-    this.mar = response.data.IsMarried;
-    console.log("dfdfdfdfd", this.mar);
+    this.form.IsMarried = response.data.IsMarried.toString();
+    this.form.Gender = response.data.Gender.toString();
 
-    // if(  response.data.IsMarried){
-    //   this.mar = "متاهل";
-    //   console.log ("dfdfdfdfd", this.mar)
-
-    // }
-
-    this.form.BirthDate = response.data.BirthDate;
+    this.form.BirthDate = response.data.BirthDate.toString();
 
     this.form.Degree = response.data.Degree;
+    console.log("Degree: ", this.form.Degree);
+
     this.form.JobType = response.data.JobType;
 
     this.form.Email = response.data.Email;
@@ -268,13 +285,39 @@ export default {
     this.form.Address = response.data.Address;
 
     this.form.CityId = response.data.CityId;
+
+    this.form.ProvinceId = response.data.ProvinceId;
+
+    console.log("ProvinceId: ", this.form.ProvinceId);
+
     this.form.ProfilePictrue = response.data.ProfilePictrue;
+
+    let res = await axios.get(
+      `http://localhost:8080/api/City/GetByProvinceId/${this.form.ProvinceId}`,
+      this.form.ProvinceId,
+
+      {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      }
+    );
+    this.cities = res.data;
+    console.log("cities", res.data);
   },
 
   data() {
     return {
+      //img
+      title: "Image Upload",
+      dialog: false,
+      imageName: "",
+      imageUrl: "",
+      imageFile: {},
+
       Province: [],
       cities: [],
+      files: [],
 
       //form
       form: {
@@ -292,7 +335,7 @@ export default {
         Address: "",
         CityId: "",
         ProfilePictrue: "",
-        ostan: "",
+        ProvinceId: "",
       },
 
       mar: null,
@@ -307,21 +350,58 @@ export default {
         { Name: "مجرد", Value: "false" },
       ],
 
+      Degree: [
+        { Name: "دیپلم", Value: 0 },
+        { Name: "کاردانی", Value: 1 },
+        { Name: "لیسانس", Value: 2 },
+        { Name: "فوق لیسانس", Value: 3 },
+        { Name: "دکترا", Value: 4 },
+      ],
+
+      JobType: [
+        { Name: "کارمند", Value: 0 },
+        { Name: "شغل آزاد", Value: 1 },
+      ],
+
       show4: false,
-      rules: {
-        required: (value) => !!value || "Required.",
-        min: (v) => v.length >= 8 || "Min 8 characters",
-      },
     };
   },
 
+  watch: {
+    getOstan() {},
+  },
+
   methods: {
+    pickFile() {
+      this.$refs.image.click();
+    },
+
+    onFilePicked(e) {
+      const files = e.target.files;
+      if (files[0] !== undefined) {
+        this.imageName = files[0].name;
+        if (this.imageName.lastIndexOf(".") <= 0) {
+          return;
+        }
+        const fr = new FileReader();
+        fr.readAsDataURL(files[0]);
+        fr.addEventListener("load", () => {
+          this.imageUrl = fr.result;
+          this.imageFile = files[0]; // this is an image file that can be sent to server...
+          this.sendImg();
+        });
+      } else {
+        this.imageName = "";
+        this.imageFile = "";
+        this.imageUrl = "";
+      }
+    },
+
     numberWithCommas(x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
 
     async updateCust() {
-
       await axios
         .post(`http://localhost:8080/api/Customer/Update`, this.form, {
           headers: {
@@ -336,6 +416,53 @@ export default {
         .catch((e) => {
           this.errors.push(e);
         });
+    },
+
+    //   async submitUpdateUserImage() {
+
+    //   let vm = this._updateUserImageVue;
+    //   var formData = new FormData();
+    //   var fileUpload = vm.$refs.fileUpload;
+
+    //   if (fileUpload && fileUpload["files"].length > 0) {
+    //     formData.append(fileUpload["name"], fileUpload["files"][0]);
+    //   }
+    //   else
+    //   {
+    //     await this.notify({vm , data : {Message : "فایل باید وارد شود." , MessageType : MessageType.Error ,Obj : null }})
+    //     return ;
+    //   }
+    //   return axios({
+    //     method: "post",
+    //     url: `${baseUrl}/UpdateUserImage`,
+    //     data: formData,
+    //     headers: { "Content-Type": "multipart/form-data" }
+    //   })
+
+    // },
+
+    async sendImg() {
+      console.log(this.imageFile);
+
+      await axios.post(`http://localhost:8080/api/Customer/UpdateUserImage`, {
+        headers: { "Content-Type": "multipart/form-data" },
+        data: this.imageFile,
+      });
+
+      // post({
+      //   method: "post",
+      //   url: `http://localhost:8080/api/Customer/UpdateUserImage`,
+      //   data: this.imageFile,
+      //   headers: { "Content-Type": "multipart/form-data" },
+      // });
+
+      // .then((response) => {
+      //   console.log(response);
+
+      // })
+      // .catch((e) => {
+      //   this.errors.push(e);
+      // });
     },
   },
 };
